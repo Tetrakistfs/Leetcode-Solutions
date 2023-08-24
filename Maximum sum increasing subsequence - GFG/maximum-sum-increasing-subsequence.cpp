19eff944139bf96379d184364038365bae908e41
Tetrakis <78267844+Tetrakistfs@users.noreply.github.com>
2023-08-24 23:17:56 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -5,40 +5,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	int func(int idx, int previdx, vector<vector<int>>& dp, int arr[], int n) {
-	    if(idx == n)    return 0;
+// 	int func(int idx, int previdx, vector<vector<int>>& dp, int arr[], int n) {
+// 	    if(idx == n)    return 0;
 	    
-	    if(dp[idx][previdx+1] != -1)    return dp[idx][previdx+1];
+// 	    if(dp[idx][previdx+1] != -1)    return dp[idx][previdx+1];
 	    
-	    int maxi = func(idx+1, previdx, dp, arr, n);
-	    if(previdx == -1 || arr[idx] > arr[previdx]) {
-	        maxi = max(maxi, arr[idx] +  func(idx+1, idx, dp, arr, n));
-	    }
-	    return dp[idx][previdx+1] = maxi; 
-	}
+// 	    int maxi = func(idx+1, previdx, dp, arr, n);
+// 	    if(previdx == -1 || arr[idx] > arr[previdx]) {
+// 	        maxi = max(maxi, arr[idx] +  func(idx+1, idx, dp, arr, n));
+// 	    }
+// 	    return dp[idx][previdx+1] = maxi; 
+// 	}
 	
 	
 	int maxSumIS(int arr[], int n)  
 	{  
 	    // Your code goes here
-	    vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-	    return func(0, -1, dp, arr, n);
-	   // vector<int> res;
-	   // res.push_back(arr[0]);
-	   // for(int i = 0; i < n; i++) {
-	   //     if(arr[i] > res.back()) {
-	   //         res.push_back(arr[i]);
-	   //     }
-	   //     else if(arr[i] < res.back()) {
-	   //         vector<int>::iterator it = lower_bound(res.begin(), res.end(), arr[i]);
-	   //         if(it != res.end())
-	   //             res[it-res.begin()] = arr[i];
-	   //     }
-	   // }
-	   // int ans = 0;
-	   // for(auto i : res)
-	   //     ans += i;
-	   // return ans;
+	    vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+	   // return func(0, -1, dp, arr, n);
+	   for(int idx = n-1; idx >= 0; idx--) {
+	        for(int previdx = idx-1; previdx >= -1; previdx--) {
+	            int maxi = dp[idx+1][previdx+1];
+	            if(previdx == -1 || arr[idx] > arr[previdx])
+	                maxi = max(maxi, arr[idx] + dp[idx+1][idx+1]);
+	           
+	            dp[idx][previdx+1] = maxi;
+	        }
+	   }
+	   
+	   return dp[0][0];
 	}  
 };
 
